daa73cf1e2e191157340f9d09abf82345026b2ce
Chris Kennelly <ckennelly@ckennelly.com>
2020-12-22 00:38:20 -0500

    2020: Day 21

---
+++ cpp/21/21a.cc
@@ -0,0 +1,101 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <regex>
+#include <set>
+#include <string>
+#include <vector>
+
+#include "absl/container/flat_hash_map.h"
+#include "absl/container/flat_hash_set.h"
+#include "absl/strings/ascii.h"
+#include "absl/strings/numbers.h"
+#include "absl/strings/match.h"
+#include "absl/strings/str_format.h"
+#include "absl/strings/str_join.h"
+#include "absl/strings/str_split.h"
+#include "absl/types/optional.h"
+
+std::string readAll() {
+    std::stringstream buffer;
+    buffer << std::cin.rdbuf();
+    return buffer.str();
+}
+
+struct Records {
+    int seen;
+    std::map<std::string, int> allergies;
+};
+
+struct Parsed {
+    absl::flat_hash_set<std::string> ingredients;
+    absl::flat_hash_set<std::string> allergies;
+};
+
+int main(int argc, char **argv) {
+    std::vector<std::string> lines =
+        absl::StrSplit(readAll(), "\n", absl::SkipEmpty());
+
+    std::vector<Parsed> parsed;
+    absl::flat_hash_set<std::string> ingredients, allergies;
+    absl::flat_hash_map<std::string, int> seen;
+
+    for (const auto& line : lines) {
+        const size_t pos = line.find('(');
+        std::string raw_ingredients, raw_allergies;
+        if (pos == std::string::npos) {
+            raw_ingredients = line;
+        } else {
+            raw_ingredients = line.substr(0, pos);
+            const size_t offset = pos + strlen("(contains ");
+            raw_allergies = line.substr(offset, line.size() - offset - 1);
+        }
+
+        Parsed p;
+        p.ingredients = absl::StrSplit(raw_ingredients, ' ', absl::SkipEmpty());
+        p.allergies = absl::StrSplit(raw_allergies, ", ", absl::SkipEmpty());
+
+        ingredients.insert(p.ingredients.begin(), p.ingredients.end());
+        allergies.insert(p.allergies.begin(), p.allergies.end());
+
+        for (const auto& ingredient : p.ingredients) {
+            seen[ingredient]++;
+        }
+
+        parsed.push_back(std::move(p));
+    }
+
+    absl::flat_hash_map<std::string, absl::flat_hash_set<std::string>> possible;
+    for (const auto& allergy : allergies) {
+        possible[allergy] = ingredients;
+    }
+
+    for (const auto& parse : parsed) {
+        for (const auto& allergy : parse.allergies) {
+            for (const auto& ingredient : ingredients) {
+                if (!parse.ingredients.contains(ingredient)) {
+                    possible[allergy].erase(ingredient);
+                }
+            }
+        }
+    }
+
+    int count = 0;
+    for (const auto& ingredient : ingredients) {
+        bool good = true;
+        for (const auto& [allergy, candidate_ingredients] : possible) {
+            if (candidate_ingredients.contains(ingredient)) {
+                good = false;
+                break;
+            }
+        }
+
+        if (good) {
+            count += seen[ingredient];
+        }
+    }
+
+    printf("%d\n", count);
+}
